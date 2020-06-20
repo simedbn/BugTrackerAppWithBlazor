2fee56335114424a422d257d1cc71f70feb490e1
Bounzaha Mohamed <simedbounzaha@gmail.com>
2020-06-20 18:21:32 +0100

    [feature] Use a seperate component code class

---
+++ BugTrackerUI/Components/BugListComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerUI.Components
+{
+    /// <summary>
+    /// The bug list component class.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase" />
+    public class BugListComponent : ComponentBase
+    {
+        /// <summary>
+        /// Gets or sets the bug service.
+        /// </summary>
+        /// <value>
+        /// The bug service.
+        /// </value>
+        [Inject]
+        public IBugService BugService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bugs.
+        /// </summary>
+        /// <value>
+        /// The bugs.
+        /// </value>
+        public List<Bug> Bugs { get; set; }
+
+        /// <summary>
+        /// Method invoked when the component is ready to start, having received its
+        /// initial parameters from its parent in the render tree.
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            Bugs = BugService.GetBugs().OrderBy(x => x.Priority).ToList();
+        }
+    }
+}
---
+++ BugTrackerUI/Pages/NewBugComponent.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerUI.Pages
+{
+    /// <summary>
+    /// The new bug component class.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase" />
+    public class NewBugComponent : ComponentBase
+    {
+        /// <summary>
+        /// Gets or sets the bug service.
+        /// </summary>
+        /// <value>
+        /// The bug service.
+        /// </value>
+        [Inject]
+        public IBugService BugService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the nav service.
+        /// </summary>
+        /// <value>
+        /// The nav service.
+        /// </value>
+        [Inject]
+        public NavigationManager NavService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the add bug.
+        /// </summary>
+        /// <value>
+        /// The add bug.
+        /// </value>
+        public Bug AddBug { get; set; } = new Bug();
+
+        /// <summary>
+        /// Handles the valid submit.
+        /// </summary>
+        protected void HandleValidSubmit()
+        {
+            BugService.AddBug(AddBug);
+            NavService.NavigateTo("");
+        }
+    }
+}
